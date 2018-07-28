b8f983662a9a3b367ba64bb23e06903de01ae5dd
Sangeetha Rajan <sangeetharajan567@gmail.com>
2018-07-28 23:56:15 +0530

    Set a recycleview for show the products of a seller

--- app/src/main/java/com/localshopper/team/localshopper/activities/SellerProductFragment.java
+++ app/src/main/java/com/localshopper/team/localshopper/activities/SellerProductFragment.java
@@ -3,17 +3,24 @@ package com.localshopper.team.localshopper.activities;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.localshopper.team.localshopper.R;
+import com.localshopper.team.localshopper.adapters.SellerProductAdapter;
+import com.localshopper.team.localshopper.models.ItemsModel;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SellerProductFragment extends Fragment {
-
+    RecyclerView sellerProductReView;
+    ArrayList<ItemsModel> iteModelArrayList;
 
     public SellerProductFragment() {
         // Required empty public constructor
@@ -25,7 +32,16 @@ public class SellerProductFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View sellerProductView = inflater.inflate(R.layout.fragment_seller_product, container, false);
+        sellerProductReView = sellerProductView.findViewById(R.id.seller_prodt_Recycle_frag);
+        iteModelArrayList = new ArrayList<>();
+        SellerProductAdapter sellerProductAdapter = new SellerProductAdapter();
+        sellerProductAdapter.setItemModelsArray(iteModelArrayList);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(sellerProductView.getContext(),
+                LinearLayoutManager.VERTICAL, false);
+        sellerProductReView.setLayoutManager(linearLayoutManager);
+        sellerProductReView.setAdapter(sellerProductAdapter);
         return sellerProductView;
+
     }
 
 }
--- app/src/main/java/com/localshopper/team/localshopper/adapters/SellerProductAdapter.java
+++ app/src/main/java/com/localshopper/team/localshopper/adapters/SellerProductAdapter.java
@@ -2,9 +2,14 @@ package com.localshopper.team.localshopper.adapters;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.localshopper.team.localshopper.R;
 import com.localshopper.team.localshopper.models.ItemsModel;
 
 import java.util.ArrayList;
@@ -21,12 +26,17 @@ public class SellerProductAdapter extends RecyclerView.Adapter<SellerProductAdap
     @NonNull
     @Override
     public SellerProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.seller_product_item, parent, false);
+        return new SellerProductViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull SellerProductViewHolder holder, int position) {
-
+        ItemsModel model = itemModelsArray.get(position);
+        holder.nameTxt.setText(model.getName());
+        holder.descriptionTxt.setText(model.getDescription());
+        holder.priceTxt.setText(String.valueOf(model.getRate()));
     }
 
     @Override
@@ -35,8 +45,25 @@ public class SellerProductAdapter extends RecyclerView.Adapter<SellerProductAdap
     }
 
     public class SellerProductViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView cancelBt;
+        ImageView contactPhotoImg;
+        TextView nameTxt;
+        TextView descriptionTxt;
+        TextView priceTxt;
+        Button reviewBt;
+        Button ratingBt;
+        Button editBt;
         public SellerProductViewHolder(View itemView) {
             super(itemView);
+            cancelBt = itemView.findViewById(R.id.cancel_img_item_SPF);
+            contactPhotoImg = itemView.findViewById(R.id.photo_img_item_SPF);
+            nameTxt = itemView.findViewById(R.id.title_txt_item_SPF);
+            descriptionTxt = itemView.findViewById(R.id.description_txt_item_SPF);
+            priceTxt = itemView.findViewById(R.id.price_txt_item_SPF);
+            reviewBt = itemView.findViewById(R.id.review_btn_item_SPF);
+            ratingBt = itemView.findViewById(R.id.rating_btn_item_SPF);
+            editBt = itemView.findViewById(R.id.edit_btn_item_SPF);
         }
     }
 }
